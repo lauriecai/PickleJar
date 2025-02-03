015b5cd619726bc945db9f6a61f1e970254ad5d5
Laurie Cai <laurie801@gmail.com>
2025-02-03 12:46:02 -0800

    Configure package dependencies

--- server/app.js
+++ server/app.js
@@ -1,3 +1,4 @@
+import "dotenv/config";
 import cors from "cors";
 import express from "express";
 import { fileURLToPath } from "url";
@@ -6,3 +7,20 @@ import mongoose from "mongoose";
 import morgan from "morgan";
 import multer from "multer";
 import path from "path";
+
+/* CONFIGURATIONS */
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express();
+
+app.use(express.json({ limit: "30mb", extended: true }));
+app.use(express.urlencoded({ limit: "30mb", extended: true }));
+
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(cors());
+
+app.use(morgan("common"));
+
+app.use("/assets", express.static(path.join(__dirname, "public/assets")));
