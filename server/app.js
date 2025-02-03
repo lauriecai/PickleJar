6e651b2f19b06da57f2fefe8039b25b579ddab7a
Laurie Cai <laurie801@gmail.com>
2025-02-03 12:40:13 -0800

    Install and import package dependencies

---
+++ server/app.js
@@ -0,0 +1,8 @@
+import cors from "cors";
+import express from "express";
+import { fileURLToPath } from "url";
+import helmet from "helmet";
+import mongoose from "mongoose";
+import morgan from "morgan";
+import multer from "multer";
+import path from "path";
