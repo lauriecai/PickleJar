9b0cfa8fffd1fac52a153350020710dd04d34ea7
Laurie Cai <laurie801@gmail.com>
2025-02-03 13:25:13 -0800

    Implement user registration with bcrypt password hashing

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -20,3 +20,37 @@ export const login = async (req, res) => {
     res.status(500).json({ error: err.message });
   }
 };
+
+/* REGISTER USER */
+export const register = async (req, res) => {
+  try {
+    const {
+      firstName,
+      lastName,
+      email,
+      password,
+      picturePath,
+      friends,
+      location,
+      occupation,
+    } = req.body;
+
+    const salt = await bcrypt.genSalt();
+    const passwordHash = await bcrypt.hash(password, salt);
+
+    const newUser = new User({
+      firstName,
+      lastName,
+      email,
+      password: passwordHash,
+      picturePath,
+      friends,
+      location,
+      occupation,
+    });
+    const savedUser = await newUser.save();
+    res.status(201).json(savedUser);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
