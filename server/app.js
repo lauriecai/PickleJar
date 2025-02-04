3bfde741393f7e41d5cf50c92ed6f3ee014c6124
Laurie Cai <laurie801@gmail.com>
2025-02-04 11:12:24 -0800

    Set up posts routes

--- server/app.js
+++ server/app.js
@@ -8,8 +8,14 @@ import morgan from "morgan";
 import multer from "multer";
 import path from "path";
 import storage from "./cloudinary/index.js";
+
 import authRoutes from "./routes/auth.js";
+import postRoutes from "./routes/posts.js";
 import userRoutes from "./routes/users.js";
+
+import { createPost } from "./controllers/posts.js";
+import { register } from "./controllers/auth.js";
+import { verifyToken } from "./middleware/auth.js";
 
 /* CONFIGURATIONS */
 const __filename = fileURLToPath(import.meta.url);
@@ -31,9 +37,14 @@ app.use("/assets", express.static(path.join(__dirname, "public/assets")));
 /* FILE STORAGE */
 const upload = multer({ storage });
 
+/* ROUTES WITH FILES */
+app.post("/auth/register", upload.single("picture"), register);
+app.post("/posts", verifyToken, upload.single("picture"), createPost);
+
 /* ROUTES */
 app.use("/auth", authRoutes);
 app.use("/users", userRoutes);
+app.use("/posts", postRoutes);
 
 /* MONGOOSE SETUP */
 const PORT = process.env.PORT || 6001;
