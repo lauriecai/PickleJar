d90412deeff04a081871847fd97d593047ef8f1c
Laurie Cai <laurie801@gmail.com>
2025-02-05 15:03:57 -0800

    Implement createPost controller to handler post creation

---
+++ server/controllers/posts.js
@@ -0,0 +1,49 @@
+import Post from "../models/Post.js";
+import User from "../models/User.js";
+
+/* CREATE */
+export const createPost = async (req, res) => {
+  try {
+    const { userId, description, postPicturePath } = req.body;
+    const user = await User.findById(userId);
+    const newPost = new Post({
+      userId,
+      firstName: user.firstName,
+      lastName: user.lastName,
+      location: user.location,
+      description,
+      userPicturePath: user.picturePath,
+      postPicturePath,
+      likes: {},
+    });
+    await newPost.save();
+
+    const posts = await Post.find();
+    res.status(201).json(posts);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
+
+/* READ */
+export const getFeedPosts = async (req, res) => {
+  try {
+  } catch (err) {
+    res.status(404).json({ message: err.message });
+  }
+};
+
+export const getUserPosts = async (req, res) => {
+  try {
+  } catch (err) {
+    res.status(404).json({ message: err.message });
+  }
+};
+
+/* UPDATE */
+export const likePost = async (req, res) => {
+  try {
+  } catch (err) {
+    res.status(404).json({ message: err.message });
+  }
+};
